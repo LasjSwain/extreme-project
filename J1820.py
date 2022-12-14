48033102e64f1fca0584a1be3e5f164ed4617291
Lars Zwaan <lars1095@hotmail.com>
2022-12-14 12:59:24 +0100

    added and slightly modifed IC tutorial

--- J1820.py
+++ J1820.py
@@ -4,10 +4,15 @@
 # plotting the spectrum of J1820, consising of self-absorbed synchrotron
 # and SSC (inverse Compton upscattering of local synchrotron photons) emission
 
+# imports used in original jet slices code
 import math
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+
+# imports used in IC tutorial
+import scipy.integrate as integrate
+from scipy.integrate import quad
 
 # leave out sys before handing in
 # used to exit before outputting rest of code, eg in testing
@@ -104,6 +109,569 @@ def intensity(q, m, const_C, B, pitch_angle, p, nu, R_t):
 # an outburst cycle (from very low tau to tau~1) , and make a plot of the resulting
 # radio/X-ray luminosity correlation.
 
+# START IC PART (PS4)
+
+# note: parts unnecessary for the final N vs hv plot have been deleted
+# constant names have been made consistent
+# some comments from the original notebook have been deleted
+
+def compton_y(pre,post):
+    return(np.mean((post-pre)/pre))
+
+def random_direction(number=None):
+    """Returns randomly oriented unit vectors.
+
+    Args: 
+        None
+        
+    Parameters:
+        number: number of random vectors to return
+
+    Returns::
+        (number,3)-element numpy array: Randomly oriented unit vectors
+    """
+
+    #
+    # This is how you draw a random number for a uniform
+    # distribution:
+    #
+
+    if number is None:
+        number=1
+
+    phi=2.*np.pi*np.random.rand(number)
+    cos_phi=np.cos(phi)
+    sin_phi=np.sin(phi)
+    cos_theta=2.*np.random.rand(number)-1
+    sin_theta=np.sqrt(1 - cos_theta**2)
+    return((np.array([sin_theta*cos_phi,sin_theta*sin_phi,cos_theta])).transpose())
+
+def photon_origin(number=None):
+    """Returns emission location of a photon
+    """
+    if number is None:
+        number=1
+    return(np.zeros([number,3]))
+
+def draw_seed_photons(mc_parms,number=None):
+    """Returns a single seed photon
+    
+    Args:
+        mc_parms (dictionary): MC parameters
+    
+    Parameters:
+        number (integer): number of photons to return
+        
+    Returns:
+        (number x 4)numpy array: Photon momentum 4-vectors
+        (number x 3)numpy array: Initial photon positions
+    """
+
+    if number is None:
+        number=1
+    x_seed=photon_origin(number=number)
+    n_seed=random_direction(number=number)
+    hnu=mc_parms['hnu_dist'](mc_parms,number=number)
+    p_seed=(np.array([hnu,hnu*n_seed[:,0],hnu*n_seed[:,1],hnu*np.abs(n_seed[:,2])])).transpose()/c
+    return(p_seed,x_seed)
+
+#
+# Write a function thar returns tau(P)
+#
+
+def tau_of_scatter():
+    """Calculates optical depth a photon traveled to before interacting, given probability
+    
+    Args:
+        None
+        
+    Returns:
+        real: Optical depth as function of P
+
+    """
+    
+    # First, draw your random probability P
+    tau=-np.log(np.random.rand())
+    return(tau)
+
+#
+# Calculate the distance s the photon travels after scattering
+#
+
+def distance_of_scatter(mc_parms):
+    """Calculates the distance that corresponds to an optical depth tau   
+
+    Args:
+        tau (real): optical depth photon travels before scattering occurs
+        mc_parsm (dictionary): MC parameters
+
+    Returns:
+        real: distance
+
+    """
+
+    tau=tau_of_scatter()
+    electron_density=mc_parms['tau']/mc_parms['H']/sigma_T
+    distance=tau/sigma_T/electron_density
+    
+    return(distance)
+
+def scatter_location(x_old,p_photon,mc_parms):
+    """This function goes through the steps of a single scattering
+
+    Args:
+        x_old.   (three-element numpy array): holds the position
+        p_photon (four-element numpy array): the in-coming photon four-momentum
+        mc_parms (dictionary): the simulation parameters
+    
+    Returns:
+        three-element numpy array: scattering location
+    """
+    
+    # ...path-length:
+    distance = distance_of_scatter(mc_parms)
+    
+    # ...in direction:
+    photon_direction=p_photon[1:]/p_photon[0]
+    
+    # Update photon position with the new location
+    x_new = x_old + distance*photon_direction
+    
+    return(x_new)
+
+
+def draw_electron_velocity(mc_parms,p_photon):
+    """Returns a randomized electron velocity vector for inverse 
+       Compton scattering, taking relativistic foreshortening of the
+       photon flux in the electron frame into account
+       
+       Args:
+           mc_parms (dictionary): Monte-Carlo parameters
+           p_photon (4 dimentional np array): Photon 4-momentum
+           
+       Returns:
+           3-element numpy array: electron velocity
+    """
+    v=mc_parms['v_dist'](mc_parms)
+    n=draw_electron_direction(v,p_photon)
+    return(v*n)
+
+def draw_electron_direction(v,p_photon):
+    """Draw a randomized electron direction, taking account of the
+       increase in photons flux from the foward direction, which
+       effectively increases the cross section for forward scattering.
+       
+       Args:
+            v (real): electron speed
+            p_photon (4 element numpy array): photon 4-momentum
+            
+       Returns:
+           3-element numpy array: randomized electron velocity vector
+    """
+    phi=2.*np.pi*np.random.rand()
+    cosp=np.cos(phi)
+    sinp=np.sin(phi)
+    cost=mu_of_p_electron(v/c,np.random.rand())
+    sint=np.sqrt(1 - cost**2)
+    
+    n_1=p_photon[1:]/p_photon[0]
+    if (np.sum(np.abs(n_1[1:2])) != 0):
+        n_2=np.cross(n_1,np.array([1,0,0]))
+    else:
+        n_2=np.cross(n_1,np.array([0,1,0]))
+    n_2/=np.sqrt(np.sum(n_2**2))
+    n_3=np.cross(n_1,n_2)
+    
+    # express new vector in old base
+    n_new=(n_2*cosp+n_3*sinp)*sint + n_1*cost
+    return(n_new/np.sqrt(np.sum(n_new**2)))
+
+def mu_of_p_electron(beta,p):
+    """Invert probability for foreshortened effective
+       Thomson scattering cross section, with
+    
+       P = 
+       
+       Args:
+           beta (real): v/c for electron
+           p: probability value between 0 and 1
+           
+       Returns:
+           real: cos(theta) relative to photon direction
+    """
+    mu=1/beta-np.sqrt(1/beta**2 + 1 - 4*p/beta + 2/beta)
+    return(mu)
+
+#
+# Functions for general Lorentz transform
+#
+
+def lorentz_transform(p,v):
+    """Returns general Lorentz transform
+
+    Args:
+        p (four-element numpy array): input four-vector
+        v (three-element numpy array): the 3-velocity of the frame we want to transform into
+
+    Returns:
+        four-element numpy array: the transformed four-vector
+    """
+
+    beta=np.sqrt(np.sum(v**2))/c
+    beta_vec=v/c
+    gamma=1./np.sqrt(1. - beta**2)
+    matrix=np.zeros((4,4))
+    matrix[0,0]=gamma
+    matrix[1:,0]=-gamma*beta_vec
+    matrix[0,1:]=-gamma*beta_vec
+    matrix[1:,1:]=(gamma-1)*np.outer(beta_vec,beta_vec)/beta**2
+    for i in range(1,4):
+        matrix[i,i]+=1
+    return(np.dot(matrix,p))
+
+#
+# Calculate the scattering angle for a probability between 0 and 1
+#
+
+def cos_theta_thomson(p):
+    """Invert P(<\theta) to calculate cos(theta)
+        
+        Args:
+            p (real): probability between 0 and 1
+            
+        Returns:
+            real: scattering angle drawn from Thomson distribution
+    """
+    a=-4 + 8*p
+    b=a**2 + 4
+    return((np.power(2,1/3)*np.power(np.sqrt(b)-a,2/3)-2)/
+           (np.power(2,2/3)*np.power(np.sqrt(b)-a,1/3)))
+
+#
+# Here we perform the Thomson scattering part of inverse Compton scatterin
+#
+
+def thomson_scatter(p_photon):
+    """This function performs Thomson scattering on a photon
+    
+    Args:
+        p_photon (4-element numpy array): Incoming photon four-vector
+        
+    Returns:
+        4-element numpy array: Scattered photon four-vector
+    """
+    
+    n_1=p_photon[1:]/p_photon[0]
+    if (np.sum(np.abs(n_1[1:2])) != 0):
+        n_2=np.cross(n_1,np.array([1,0,0]))
+    else:
+        n_2=np.cross(n_1,np.array([0,1,0]))
+    n_2/=np.sqrt(np.sum(n_2**2))
+    n_3=np.cross(n_1,n_2)
+
+    # scattering is uniform in phi
+    phi=2.*np.pi*np.random.rand()
+    cosp=np.cos(phi)
+    sinp=np.sin(phi)
+    
+    # draw cos_theta from proper distribution
+    cost=cos_theta_thomson(np.random.rand())
+    sint=np.sqrt(1 - cost**2)
+    
+    # express new vector in old base
+    n_new=(n_2*cosp+n_3*sinp)*sint + n_1*cost
+    n_new/=np.sqrt(np.sum(n_new**2))
+    
+    # return scatterd 4-momentum vector
+    return(np.array(p_photon[0]*np.array([1,n_new[0],n_new[1],n_new[2]])))
+
+#
+# Compute the new photon four-momentum after inverse Compton scattering
+#
+# This takes three steps:
+#
+# 1. Lorentz transform to electron frame
+#
+# 2. Randomize the photon direction, but keep its energy unchanged (Thomson scattering)
+#
+# 3. Lorentz transform back to the observer's frame
+#
+
+def inverse_compton_scatter(p_photon,mc_parms):
+    """This function performs an iteration of inverse Compton scattering off an electron of velocity v_vec.
+    
+    Args:
+        p_photon (four element numpy array): input photon four-momentum
+        v_vec (three element numpy array): 3-velocity vector of the scattering electron
+        
+    Returns:
+        four-element numpy array: scattered photon four-momentum in observer's frame
+    """
+    
+    # throw the dice one more time to draw a random electron velocity
+    velocity=draw_electron_velocity(mc_parms,p_photon)
+    # first, transform to electron frame
+    p_photon_prime=lorentz_transform(p_photon,velocity)
+
+    # Thomson scatter
+    p_out_prime=thomson_scatter(p_photon_prime)
+    
+    # transform back to observer frame
+    return(lorentz_transform(p_out_prime,-velocity))
+
+def monte_carlo(mc_parms):
+    """Perform a simple Monte-Carlo simulation
+
+    Args:
+       mc_parms (dictionary): Monte-Calro parameters
+    
+    Returns:
+        numpy array: List of escaped photon energies
+        numpy array: Lost of seed energies of all escaping photons
+    """
+    
+    # arrays to store initial and final photon energies
+    hnu_seed=np.zeros(mc_parms['n_photons'])
+    hnu_scattered=hnu_seed.copy()
+
+    # draw our seed-photon population. Much faster to do this once for all photons
+    p_photons,x_photons=draw_seed_photons(mc_parms,number=mc_parms['n_photons'])
+   
+    # run the scattering code n_photons times
+    for p_photon,x_photon,i in zip(p_photons,x_photons,range(mc_parms['n_photons'])):
+        # initial photon four-momentum
+        # store seed photon energy for future use (calculating Compton-y parameter)
+        hnu_seed[i]=p_photon[0]*c
+
+        # keep scattering until absorbed or escaped
+        scattered=True
+        while (scattered):
+            # find next scattering location
+            x_photon = scatter_location(x_photon,p_photon,mc_parms)
+            # if it's inside the corona, perform inverse Compton scatter
+            if (x_photon[2]>=0 and x_photon[2]<=mc_parms['H']):
+                p_photon=inverse_compton_scatter(p_photon,mc_parms)
+            else:
+                scattered=False
+                if (x_photon[2]<=0):
+                    p_photon*=0
+
+        # store the outgoing photon energy in the array
+        hnu_scattered[i]=p_photon[0]*c
+
+    # only return escaped photons and their seed energy
+    return(hnu_scattered[hnu_scattered > 0],hnu_seed[hnu_scattered > 0])
+
+def plot_mc(mc_parms,bins=None,xlims=None):
+    """Run an MC simulation and plot a histogram of the output
+    
+    Args:
+        mc_parms (dictionary): Monte-Carlo parameters
+    
+    Paramters:
+        bins (numpy array): Optional spectral bins
+        xlims (2-element list, real): plot-limits
+    
+    Returns:
+        numpy array: The energies of all photons escaping the corona
+        numpy array: The seed-energies of the escaping photons
+    """
+    
+    
+    # Now run simulation and normalize all outgoing photon energies 
+    # so we can investigate energy gains and losses
+    hnu_scattered,hnu_seeds=np.array(monte_carlo(mc_parms))/mc_parms['kt_seeds'] 
+    
+    if (xlims is None):
+        xlims=[hnu_scattered.min(),hnu_scattered.max()]    
+    if (bins is None):
+        bins=np.logspace(np.log10(xlims[0]),np.log10(xlims[1]),num=100)
+    else:
+        bins=np.logspace(np.log10(xlims[0]),np.log10(xlims[1]),num=bins)
+
+    fig=plt.figure()
+    plt.hist(hnu_scattered,bins=bins,log=True,
+             label=r'$\tau=${:4.1f}'.format(mc_parms['tau']))
+    plt.xscale('log')
+    plt.xlim(xlims[0],xlims[1])
+    plt.xlabel(r'$h\nu/h\nu_{0}$',fontsize=20)
+    plt.ylabel(r'$N(h\nu)$',fontsize=20)
+    plt.legend()
+    plt.show()
+    print('Fraction of escaping photons: {0:5.3e}\n'.format(hnu_scattered.size/mc_parms['n_photons']))
+    print('Compton y parameter: {0:5.3e}\n'.format(compton_y(hnu_seeds,hnu_scattered)))
+    return(hnu_scattered,hnu_seeds)
+
+#
+# Run an example of a Monte Carlo simulation for optical depth 0.1
+#
+
+# mc_parms={'n_photons':100000,         # start somewhat small and go up
+#           'kt_seeds':1.6e-9,         # 1 keV input photons
+#           'H':1e7,                   # say H ~ R, and R ~ 100 R_g ~ 3e7 cm
+#           'velocity':3e9,            # 10% speed of light--pretty hot
+#           'tau':0.1,                 # tau ~ 0.1: Small-ish optical depth
+#           'v_dist':f_of_v_mono,      # name of velocity distribution function
+#           'hnu_dist':f_of_hnu_mono,  # name of photon distribution function
+#          }
+# hnu_scattered,hnu_seeds=plot_mc(mc_parms,bins=100)
+
+#
+# Proper Maxwellian velocity distribution
+#
+
+def f_of_v_maxwell(mc_parms):
+    """Returns a single randomly drawn velocity from distribution function
+    
+    Args:
+        mc_parms (dictionary): Monte-Carlo parameters
+
+    Returns:
+        real: electron velocity drawn from distribution
+    """
+    
+    # we need to draw x, y, and z velocity from a Maxwell-Boltzmann distribution'
+    # and calculate the resulting speed. This is a non-relativistic Maxwelleian,
+    # so me must truncate it below c.
+    
+    v=3e10 # needed for clipping...
+    while v >= c:
+        v=np.sqrt(mc_parms['kt_electron']/(m_e))*np.sqrt(np.sum((np.random.normal(0,1,3))**2))
+    
+    return(v)
+
+#
+# For comparison, let's define a Maxwellian
+#
+
+def maxwellian(v,kT):
+    """Calculate a non-relativistic Maxwellian
+    
+    Args:
+        v (real): Velocity
+        kT (real): temperature in energy units
+    
+    Returns:
+        value of the Maxwellian at v
+    """    
+    return(v**2*np.sqrt(2/np.pi*(m_e/kT)**3)*np.exp(-(m_e*v**2/(2*kT))))
+
+#
+# In case you want to improve on the seed photon distribution, this will be helpful
+# Note that the photon distribution function goes as nu^2, not nu^3
+#
+
+def f_planck(x):
+    """Photon distribution function of a Planck distribution
+    
+    Args:
+        x (real): photon energy e in untis of kT
+        
+    Returns:
+        real: differential photon number dN/de at energy e
+    """
+    norm=2.4041138063192817
+    return x**2/(np.exp(x)-1)/norm
+
+def p_planck(hnu=None):
+    """Numerical integration of cumulative planck PDF (to be inverted)
+    
+    Parameters:
+        hnu (numpy array): bins of photon energy e in untis of kT
+        
+    Returns:
+        numpy array: cumulative photon PDF as function of hnu
+        numpy array: hnu values used for PDF
+    """
+    if (hnu is None):
+        number=1000
+        hnu=np.append(np.linspace(0,1-1./number,number),np.logspace(0,4,number))
+
+    p=np.zeros(2*number)
+    for i in range(1,2*number):
+        p[i]=((quad(f_planck,0,hnu[i]))[0])
+    return (p,hnu)
+
+def hnu_of_p_planck(number=None,pdf=None,hnu=None):
+    """Numerically invert Planck PDF
+    
+    Args:
+        None
+        
+    Parameters:
+        planck_pdf (numpy array): Previously calculated Planck PDF
+        planck_hnu (numpy array): energy grid for PDF
+        number (interger): Number of photon energies to generate
+        
+    Returns:
+        numpy array: energies corresponding to p
+        numpy array: cumulative PDF userd to calculate e
+        numpy array: hnu grid used to calculate PDF
+    """
+    if number is None:
+        number=1
+    if (pdf is None):
+        pdf,hnu=p_planck()
+
+    e_phot=np.interp(np.random.rand(number),pdf,hnu)
+    return(e_phot,pdf,hnu)
+
+#
+# Looks good?
+#
+# Here is a new seed photon distribution to replace the one we used above.
+# All you have to do is compile this function and change
+# mc_parms['hnu_prob']=f_og_hnu_planck
+#
+
+def f_of_hnu_planck(mc_parms,number=None,pdf=None,energies=None):
+    """Returns randomly drawn photon energy from a Planck distribution
+    
+    Args:
+        mc_parms (dictionary): Monte-Carlo parameters
+
+    Parameters:
+        pdf (numpy array): Previously calculated Planck PDF
+        hnu (numpy array): energy grid for PDF
+
+    Returns:
+        numpy array: seed photon energies drawn from photon distribution
+    """
+    
+    if number is None:
+        number=1
+    if (pdf is None):
+        e,pdf,energies=hnu_of_p_planck(number=number)
+    else:
+        e,pdf,energies=hnu_of_p_planck(number=number,pdf=pdf,hnu=energies)        
+    e*=mc_parms['kt_seeds']
+    
+    return(e)
+
+#
+# Looking good?
+#
+# Now run an exaple Monte Carlo simulation with a Maxwellian electron
+# distribution.
+#
+
+mc_parms={'n_photons':10000,            # start somewhat small and go up
+          'kt_seeds':1.6e-9,            # 1 keV input photons
+          'H':1e7,                      # say H ~ R, and R ~ 100 R_g ~ 3e7 cm
+          'velocity':3e9,               # 10% speed of light--pretty hot
+          'tau':0.1,                    # tau ~ 0.1: Small-ish optical depth
+          'kt_electron':3.2e-8,         # electron temperature: 20 keV
+          'v_dist':f_of_v_maxwell,      # name of photon distribution function
+          'hnu_dist':f_of_hnu_planck,     # name of photon distribution function
+         }
+
+# Let's make mc_parms consistent
+mc_parms['velocity']=np.sqrt(mc_parms['kt_electron']/(m_e)) # thermal speed
+
+hnu_scattered,hnu_seeds=plot_mc(mc_parms)
+
+# START CONICAL JET PART (PS3)
+
 def conical_jet():
 
     # "divided into ~10-20 slices"
@@ -173,4 +741,4 @@ def conical_jet():
 
     return
 
-conical_jet()
+# conical_jet()
